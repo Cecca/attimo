882fe96fea01c1b902c6570de0522b16f7c9347b
Matteo Ceccarello <mceccarello@unibz.it>
2021-08-19 11:29:34 +0200

    Larger benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -4,8 +4,8 @@ use attimo::{lsh::*, types::WindowedTimeseries};
 
 pub fn bench_hash_ts(c: &mut Criterion) {
     let w = 200;
-    let ts = WindowedTimeseries::gen_randomwalk(1000, w, 12345);
-    let hasher = Hasher::new(w, 32, 100, 10.0, 12345);
+    let ts = WindowedTimeseries::gen_randomwalk(10000, w, 12345);
+    let hasher = Hasher::new(w, 32, 200, 10.0, 12345);
     c.bench_function("hash time series", |b| b.iter(|| HashCollection::from_ts(&ts, &hasher)));
 }
 
