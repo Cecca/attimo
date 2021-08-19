7e47b55e6a782fb4036386d62c054a604667bbe4
Matteo Ceccarello <mceccarello@unibz.it>
2021-08-19 11:56:40 +0200

    Add benchmark for time series construction

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,6 +1,24 @@
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use attimo::{lsh::*, types::WindowedTimeseries};
+
+pub fn bench_construct_ts(c: &mut Criterion) {
+    use rand::prelude::*;
+    use rand_distr::StandardNormal;
+    use rand_xoshiro::Xoroshiro128Plus;
+
+    let n = 10000;
+
+    let rng = Xoroshiro128Plus::seed_from_u64(12344);
+    let mut ts: Vec<f64> = rng.sample_iter(StandardNormal).take(n).collect();
+    for i in 1..n {
+        ts[i] = ts[i-1] + ts[i];
+    }
+
+    let w = 200;
+
+    c.bench_function("construct windowed time series", |b| b.iter(|| WindowedTimeseries::new(ts.clone(), black_box(w))));
+}
 
 pub fn bench_hash_ts(c: &mut Criterion) {
     let w = 200;
