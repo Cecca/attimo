d0526a862aa3efce8064a363fd293a770130a914
Matteo Ceccarello <mceccarello@unibz.it>
2021-10-11 09:03:08 +0200

    Faster file reading

--- src/load.rs
+++ src/load.rs
@@ -10,13 +10,29 @@ pub fn loadts<P: AsRef<Path>>(path: P, prefix: Option<usize>) -> Result<Vec<f64>
     let start = Instant::now();
     let to_take = prefix.unwrap_or(usize::MAX);
     let f = File::open(path.as_ref()).with_context(|| format!("reading {:?}", path.as_ref()))?;
-    let f = BufReader::new(f);
-    let res = Ok(f.lines()
-        .take(to_take)
-        .map(|l| l.unwrap())
-        .filter(|l| !l.is_empty())
-        .map(|l| l.parse::<f64>().unwrap())
-        .collect());
+    let mut f = BufReader::new(f);
+    let mut res: Vec<f64> = Vec::with_capacity(10000000);
+    let mut buf = String::new();
+    let mut cnt = 0;
+    while cnt < to_take {
+        buf.clear();
+        match f.read_line(&mut buf) {
+            Ok(0) => break, // EOF reached
+            Ok(_) => {
+                if !buf.is_empty(){
+                    res.push(fast_float::parse_partial(&buf).with_context(|| format!("parsing `{}`", buf))?.0);
+                }
+            }
+            Err(e) => anyhow::bail!(e),
+        }
+        cnt += 1;
+    }
+    // for l in f.lines().take(to_take) {
+    //     let l = l.unwrap();
+    //     if !l.is_empty() {
+    //         res.push(fast_float::parse(l).unwrap());
+    //     }
+    // }
     slog_scope::info!("input reading"; "tag" => "profiling", "time_s" => start.elapsed().as_secs_f64());
-    res
+    Ok(res)
 }
