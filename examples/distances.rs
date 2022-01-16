2b0e062a1fb959a1afbfd643446d19c6fa9940b2
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2022-01-16 07:06:20 +0100

    Add sampler for pairwise distances

---
+++ examples/distances.rs
@@ -0,0 +1,69 @@
+use anyhow::Result;
+use argh::*;
+use attimo::distance::zeucl;
+use attimo::load::loadts;
+use attimo::timeseries::*;
+use indicatif::{ProgressBar, ProgressStyle};
+use rand::prelude::*;
+use rand_distr::Uniform;
+use rayon::prelude::*;
+use std::io::{prelude::*, BufWriter};
+use std::path::PathBuf;
+
+#[derive(FromArgs)]
+/// Compute the Local Intrinsic Dimensionality of datasets
+struct Args {
+    #[argh(option, short = 'w')]
+    /// the window size
+    window: usize,
+    #[argh(option)]
+    /// path to file
+    path: PathBuf,
+    #[argh(option)]
+    /// path to output
+    output: PathBuf,
+    #[argh(option)]
+    /// number of pairs to sample
+    samples: usize,
+}
+
+fn main() -> Result<()> {
+    let args: Args = argh::from_env();
+    let ts = WindowedTimeseries::new(loadts(&args.path, None)?, args.window, false);
+
+    let mut rng = rand_xoshiro::Xoshiro256StarStar::seed_from_u64(1234);
+    let uniform = Uniform::new(0, ts.num_subsequences());
+    let pairs: Vec<(usize, usize)> = (0..(10 * args.samples))
+        .filter_map(|_| {
+            let i = uniform.sample(&mut rng);
+            let j = uniform.sample(&mut rng);
+            if (i as isize - j as isize).abs() < ts.w as isize {
+                None
+            } else {
+                Some((i, j))
+            }
+        })
+        .take(args.samples)
+        .collect();
+
+    let pbar = ProgressBar::new(args.samples as u64).with_style(
+        ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {msg} {bar:40.cyan/blue} {pos:>7}/{len:7}"),
+    );
+
+    let distances: Vec<f64> = pairs
+        .into_par_iter()
+        .map(|(i, j)| zeucl(&ts, i, j))
+        .collect();
+
+    pbar.finish_and_clear();
+
+    let output_path = args.output;
+    let mut output_file = BufWriter::new(std::fs::File::create(output_path)?);
+    writeln!(output_file, "w, distance")?;
+    for d in distances {
+        writeln!(output_file, "{},{}", ts.w, d)?;
+    }
+
+    Ok(())
+}
