6ca45b2fe40d240721b3a2ee574d12be46dea6d5
Matteo Ceccarello <mceccarello@unibz.it>
2022-01-25 16:35:45 +0100

    Bump version number

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::sync::atomic::AtomicBool;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 
-const VERSION: u32 = 15;
+const VERSION: u32 = 16;
 
 #[global_allocator]
 static A: CountingAllocator = CountingAllocator;
