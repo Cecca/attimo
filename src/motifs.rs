1e716e68248de319ab122b7273dfe606f59141d1
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2022-06-28 10:50:06 +0200

    Add MotifIterator class

--- src/motifs.rs
+++ src/motifs.rs
@@ -196,6 +196,21 @@ impl TopK {
             .map(|m| *m)
     }
 
+    pub fn first_outstanding_mut(&mut self) -> Option<&mut Motif> {
+        self.top
+            .iter_mut()
+            .filter(|m| m.elapsed.is_none())
+            .next()
+    }
+
+    pub fn first_outstanding_dist(&self) -> Option<f64> {
+        self.top
+            .iter()
+            .filter(|m| m.elapsed.is_none())
+            .next()
+            .map(|m| m.distance)
+    }
+
     pub fn last_confirmed(&self) -> Option<Motif> {
         self.top
             .iter()
@@ -227,6 +242,224 @@ impl TopK {
     pub fn add_all(&mut self, other: &mut TopK) {
         for m in other.top.drain(..) {
             self.insert(m);
+        }
+    }
+}
+
+/// Iterator over the motifs of a time series, which are returned in increasing order of distance
+/// 
+/// # Examples
+/// 
+/// ```
+/// use attimo::timeseries::WindowedTimeseries;
+/// use attimo::motifs::*;
+/// 
+/// let w = 100;
+/// // Load some data, the first 10000 data points of an ECG trace
+/// let ts = WindowedTimeseries::new(attimo::load::loadts("data/ECG.csv.gz", Some(10000)).unwrap(), w, false);
+/// let mut motifs = MotifIterator::new(&ts, 10, 100, 0.01, 1234);
+/// let m = motifs.next().unwrap();
+/// assert_eq!(m.idx_a, 616);
+/// assert_eq!(m.idx_b, 2780);
+/// assert_eq!(m.distance, 0.17614364917722336);
+/// ```
+pub struct MotifIterator<'a> {
+    ts: &'a WindowedTimeseries,
+    repetitions: usize,
+    delta: f64,
+    start: Instant,
+    exclusion_zone: usize,
+    hasher: Arc<Hasher>,
+    pools: Arc<HashCollection>,
+    prev_prefix: Option<usize>,
+    cur_prefix: usize,
+    cur_repetition: usize,
+    //// This vector holds the (sorted) hashed subsequences, and their index
+    column_buffer: Vec<(HashValue, u32)>,
+    //// This vector holds the boundaries between buckets. We reuse the allocations
+    buckets: Vec<Range<usize>>,
+    tl_top: ThreadLocal<RefCell<TopK>>,
+    top: TopK,
+}
+
+impl<'a> MotifIterator<'a> {
+    pub fn new(ts: &'a WindowedTimeseries, max_topk: usize, repetitions: usize, delta: f64, seed: u64) -> Self {
+        let start = Instant::now();
+        let exclusion_zone = ts.w;
+
+        let fft_data = FFTData::new(&ts);
+
+        let hasher_width = Hasher::estimate_width(
+            &ts,
+            &fft_data,
+            1,
+            None,
+            seed,
+        );
+        let hasher = Arc::new(Hasher::new(ts.w, repetitions, hasher_width, seed));
+        let pools = HashCollection::from_ts(ts, &fft_data, Arc::clone(&hasher));
+        let pools = Arc::new(pools);
+        drop(fft_data);
+
+
+        Self {
+            ts,
+            repetitions,
+            delta,
+            start,
+            exclusion_zone,
+            hasher,
+            pools,
+            prev_prefix: None,
+            cur_prefix: K,
+            cur_repetition: 0,
+            column_buffer: Vec::new(),
+            buckets: Vec::new(),
+            tl_top: ThreadLocal::new(),
+            top: TopK::new(max_topk, exclusion_zone)
+        }
+    }
+
+    fn stopping_condition(&self, d: f64, prefix: usize, prev_prefix: Option<usize>, repetition: usize) -> bool {
+        let p = self.hasher.collision_probability_at(d);
+        let i_half = prefix as f64 / 2.0;
+        let sqrt = (self.repetitions as f64).sqrt().ceil() as i32;
+        let j_left = repetition as i32 / sqrt;
+        let j_right = repetition as i32 % sqrt;
+        let failure_p = if let Some(previous) = prev_prefix {
+            let prev_half = previous as f64 / 2.0;
+            let lu_i = 1.0 - (1.0 - p.powf(i_half)).powi(j_left);
+            let ru_i = 1.0 - (1.0 - p.powf(i_half)).powi(j_right);
+            let lu_ip = 1.0 - (1.0 - p.powf(prev_half)).powi(j_left);
+            let ru_ip = 1.0 - (1.0 - p.powf(prev_half)).powi(j_right);
+            let ll_ip = 1.0 - (1.0 - p.powf(prev_half)).powi(sqrt - j_left);
+            let rl_ip = 1.0 - (1.0 - p.powf(prev_half)).powi(sqrt - j_right);
+            (1.0 - lu_i * ru_i)
+                * (1.0 - lu_ip * rl_ip)
+                * (1.0 - ll_ip * ru_ip)
+                * (1.0 - ll_ip * rl_ip)
+        } else {
+            let lu_i = 1.0 - (1.0 - p.powf(i_half)).powi(j_left);
+            let ru_i = 1.0 - (1.0 - p.powf(i_half)).powi(j_right);
+            1.0 - lu_i * ru_i
+        };
+        failure_p <= self.delta
+    }
+
+    fn prefix_for_distance(&self, d: f64, mut prefix: usize) -> usize {
+        let initial = prefix;
+        while prefix > 0 {
+            for rep in 0..self.repetitions {
+                if self.stopping_condition(d, prefix, Some(initial), rep) {
+                    return prefix;
+                }
+            }
+            prefix -= 1;
+        }
+        panic!()
+    }
+
+}
+
+impl<'a> Iterator for MotifIterator<'a> {
+    type Item = Motif;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            assert!(self.cur_prefix > 0, "Empty prefixes are not supported!");
+
+            // Emit the first outstanding motif, if any
+            if let Some(d) = self.top.first_outstanding_dist() {
+            if self.stopping_condition(d, self.cur_prefix, self.prev_prefix, self.cur_repetition) {
+                let outstanding = self.top.first_outstanding_mut().unwrap();
+                assert!(outstanding.distance == d);
+                outstanding.elapsed.replace(self.start.elapsed());
+                return Some(outstanding.clone())
+            }
+            }
+
+            // advance the iterator by one repetition, or decrease the prefix and reset repetition
+            if self.cur_repetition + 1 == self.repetitions {
+                self.cur_repetition = 0;
+                self.prev_prefix.replace(self.cur_prefix);
+                if let Some(d) = self.top.first_outstanding_dist() {
+                    self.cur_prefix = self.prefix_for_distance(d, self.cur_prefix);
+                } else {
+                    self.cur_prefix -= 1;
+                }
+                assert!(self.cur_prefix > 0, "Empty prefixes are not supported!");
+            } else {
+                self.cur_repetition += 1;
+            }
+
+            // Do the repetition
+            self.pools.group_subsequences(self.cur_prefix, self.cur_repetition, self.exclusion_zone, &mut self.column_buffer, &mut self.buckets);
+            let n_buckets = self.buckets.len();
+            // Each thread works on these many buckets at one time, to reduce the
+            // overhead of scheduling.
+            let chunk_size = std::cmp::max(1, n_buckets / (4 * rayon::current_num_threads()));
+
+            (0..n_buckets / chunk_size)
+                .into_par_iter()
+                .for_each(|chunk_i| {
+                    // let tl_top = tl_top.get_or(|| RefCell::new(TopK::new(topk, exclusion_zone)));
+                    let tl_top = self.tl_top.get_or(|| RefCell::new(self.top.clone()));
+
+                    for i in (chunk_i * chunk_size)..((chunk_i + 1) * chunk_size) {
+                        let bucket = &self.column_buffer[self.buckets[i].clone()];
+
+                        for (_, a_idx) in bucket.iter() {
+                            let a_idx = *a_idx as usize;
+                            // let a_already_checked = rep_bounds[a_idx].clone();
+                            // let a_hash_idx = hash_range.start + a_offset;
+                            for (_, b_idx) in bucket.iter() {
+                                let b_idx = *b_idx as usize;
+                                //// Here we handle trivial matches: we don't consider a pair if the difference between
+                                //// the subsequence indexes is smaller than the exclusion zone, which is set to `w/4`.
+                                if a_idx + self.exclusion_zone < b_idx {
+
+                                    //// We only process the pair if this is the first repetition in which
+                                    //// they collide. We get this information from the pool of bits
+                                    //// from which hash values for all repetitions are extracted.
+                                    if let Some(first_colliding_repetition) =
+                                        self.pools.first_collision(a_idx, b_idx, self.cur_prefix)
+                                    {
+                                        //// This is the first collision in this iteration, _and_ the pair didn't collide
+                                        //// at a deeper level.
+                                        if first_colliding_repetition == self.cur_repetition
+                                            && self.prev_prefix
+                                                .map(|d| {
+                                                    self.pools.first_collision(a_idx, b_idx, d).is_none()
+                                                })
+                                                .unwrap_or(true)
+                                        {
+                                            //// After computing the distance between the two subsequences,
+                                            //// we try to insert the pair in the top data structure
+                                            let d = zeucl(self.ts, a_idx, b_idx);
+                                            if d.is_finite() {
+                                                let m = Motif {
+                                                    idx_a: a_idx as usize,
+                                                    idx_b: b_idx as usize,
+                                                    distance: d,
+                                                    elapsed: None,
+                                                };
+                                                tl_top.borrow_mut().insert(m);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+
+            // Add to the top-k all the new top pairs that have been found
+            let found_motifs = self.tl_top
+                .iter_mut()
+                .flat_map(|top| top.borrow().to_vec());
+            for m in found_motifs {
+                self.top.insert(m);
+            }
         }
     }
 }
@@ -525,13 +758,6 @@ fn explore_tries(
             cnt_dist.fetch_add(rep_cnt_dists.load(Ordering::SeqCst), Ordering::SeqCst);
             pbar.inc(1);
 
-            // Add to the output all the new top pairs that have been found
-            tl_top
-                .iter_mut()
-                .for_each(|top| output.add_all(&mut top.borrow_mut()));
-            tl_top.iter_mut().for_each(|top| {
-                top.replace(output.clone());
-            });
 
             // Confirm the pairs that can be confirmed in this iteration
             output.for_each(|m| {
