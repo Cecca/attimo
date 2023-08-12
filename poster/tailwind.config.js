000e9f9607c17c4dd5e7bd0a8f8a23b332532bba
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2023-08-12 16:21:46 +0200

    start working on poster

---
+++ poster/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    // fontFamily: {
+    //   'sans': ['Lato','Helvetica'],
+    //   'serif': ['Georgia']
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
+
