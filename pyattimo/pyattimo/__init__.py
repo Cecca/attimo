bb42b3b0ee9da547d2e2396c044eaea8142e020a
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2023-10-18 14:27:50 +0200

    move load_dataset to python code

---
+++ pyattimo/pyattimo/__init__.py
@@ -0,0 +1,21 @@
+DATASETS = {
+    "astro": "https://figshare.com/ndownloader/files/36982360",
+    "ecg": "https://figshare.com/ndownloader/files/36982384",
+    "freezer": "https://figshare.com/ndownloader/files/36982390",
+    "gap": "https://figshare.com/ndownloader/files/36982396"
+}
+
+def load_dataset(dataset, prefix=None):
+    import numpy
+    from urllib.request import urlretrieve
+    import os
+
+    outfname = dataset + ".csv.gz"
+    if not os.path.isfile(outfname):
+        print("Downloading dataset")
+        urlretrieve(DATASETS[dataset], outfname)
+
+    if prefix is not None: 
+        return numpy.loadtxt(outfname)[:prefix]
+    else:
+        return numpy.loadtxt(outfname)
--- pyattimo/src/lib.rs
+++ pyattimo/src/lib.rs
@@ -260,41 +260,9 @@ pub fn motiflet(
     unreachable!()
 }
 
-#[pyfunction]
-#[pyo3(signature =(dataset, prefix = None))]
-fn load_dataset(dataset: &str, prefix: Option<usize>) -> anyhow::Result<Vec<f64>> {
-    use std::path::PathBuf;
-
-    let mapping: HashMap<String, String> = [
-        ("astro", "https://figshare.com/ndownloader/files/36982360"),
-        ("ecg", "https://figshare.com/ndownloader/files/36982384"),
-        ("freezer", "https://figshare.com/ndownloader/files/36982390"),
-        ("gap", "https://figshare.com/ndownloader/files/36982396"),
-    ]
-    .into_iter()
-    .map(|pair| (pair.0.to_owned(), pair.1.to_owned()))
-    .collect();
-    let mut outfname = PathBuf::new();
-    outfname.push(dataset);
-    outfname.set_extension("csv.gz");
-    if !outfname.is_file() {
-        let url = mapping.get(dataset).with_context(|| {
-            let available = mapping.keys().cloned().collect::<Vec<String>>().join(", ");
-            format!("Dataset {} not available [try {}]", dataset, available)
-        })?; // FIXME better error handling
-        eprintln!("Downloading file from {} into {}", url, outfname.display());
-        let mut out = std::fs::File::create(&outfname)?;
-        let resp = ureq::get(url).call().unwrap(); // FIXME handle errors in a better way
-        let mut reader = resp.into_reader();
-        std::io::copy(&mut reader, &mut out)?;
-    }
-
-    attimo::load::loadts(&outfname, prefix)
-}
-
 #[pymodule]
 fn pyattimo(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(load_dataset, m)?)?;
+    // m.add_function(wrap_pyfunction!(load_dataset, m)?)?;
     m.add_function(wrap_pyfunction!(motiflet, m)?)?;
     m.add_class::<MotifsIterator>()?;
     Ok(())
