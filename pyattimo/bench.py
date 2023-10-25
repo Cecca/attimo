b973cba55eafe9c39d013e9765c79f29210bf164
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2023-10-25 15:30:16 +0200

    update bench.py
    Now it no longer plots figures from the motiflets
    package, allowing to measure the running time

--- pyattimo/bench.py
+++ pyattimo/bench.py
@@ -1,30 +1,37 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 
+import pandas as pd
 import pyattimo
 import time
 from motiflets.plotting import *
 
 k = 20
 motif_length = 1000
-# ts = pyattimo.load_dataset("ecg", 30000)
-# ml = Motiflets(ds_name="ECG", series=ts)
-# dists, motiflets, elbow_points = ml.fit_k_elbow(k_max=k+1, motif_length=motif_length)
-# print(dists[-1])
-# print(sorted( motiflets[-1] ))
-#
-# start = time.time()
-# m = pyattimo.motiflet_brute_force(ts, motif_length, support=k)
-# end = time.time()
-# print("elapsed", end - start)
-# print(sorted(m.indices))
-# print(m.extent, m.extent * m.extent)
 
-for thousands in [30, 50, 100, 300, 500]:
+res = []
+
+for thousands in [30, 50, 100]:
     n = thousands * 1000
     ts = pyattimo.load_dataset("ecg", n)
+
+    ml = Motiflets(ds_name="ECG", series=ts)
     start = time.time()
-    m = pyattimo.motiflet_brute_force(ts, 1000, support=k)
+    dists, motiflets, elbow_points = ml.fit_k_elbow(k_max=k+1, motif_length=motif_length, 
+                                                    plot_elbows=False, plot_motifs_as_grid=False)
     end = time.time()
-    print("%5d | %.4f" % (n, (end - start)))
+    baseline = end - start
+
+    start = time.time()
+    m = pyattimo.motiflet_brute_force(ts, motif_length, support=k)
+    end = time.time()
+    rust_time = end - start
+    res.append({
+        "n": n,
+        "rust_time": rust_time,
+        "baseline": baseline
+    })
+
+res = pd.DataFrame(res)
+print(res)
 
 
