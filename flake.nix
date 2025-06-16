372bff901cabe514ec91dbe9a15d8ab42e2dcaf1
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2025-06-16 17:02:40 +0200

    chore: cleanup nix flake

--- flake.nix
+++ flake.nix
@@ -1,55 +1,40 @@
 {
-  description = "A Nix-flake-based Python development environment";
+  description = "A Nix-flake for Attimo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    devShells = forEachSupportedSystem ({pkgs}: let
-      my-quarto = pkgs.quarto.override {python3 = null;};
-    in {
-      default = pkgs.mkShell {
-        venvDir = ".venv";
-        packages = with pkgs;
-          [
-            R
-            python311
-            my-quarto
-            ruff
-            zig
-          ]
-          ++ (with pkgs.python311Packages; [
-            pip
-            venvShellHook
-            maturin
-            numpy
-            seaborn
-            matplotlib
-            soundfile
-            scikit-learn
-            joblib
-            tqdm
-            icecream
-            statsmodels
-            jupyter
-            stumpy
-            tabulate
-            pyarrow
-            # jupyter-cache
-          ])
-          ++ (with pkgs.rPackages; [
-            tidyverse
-            ggthemes
-            kableExtra
-          ]);
-      };
-    });
-  };
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        python = pkgs.python312;
+      in {
+        devShells.default = pkgs.mkShell {
+          venvDir = ".venv";
+          buildInputs = with pkgs;
+            [
+              rust-bin.nightly.latest.default
+              python
+            ]
+            ++ (with python.pkgs; [
+              venvShellHook
+              numpy
+              maturin
+              scipy
+            ]);
+        };
+      }
+    );
 }
