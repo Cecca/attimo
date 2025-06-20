d26ffcecf17eab60e68371a06c806daab740c55e
Matteo Ceccarello <matteo.ceccarello@unipd.it>
2025-06-20 11:35:21 +0200

    feat: apptainer (singularity) container
    Now we can build an apptainer container with the Python library compiled
    in, for easy deployments on HPC clusters

--- flake.nix
+++ flake.nix
@@ -19,13 +19,61 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        # The rust version (from the overlay) that we use
+        rust = pkgs.rust-bin.nightly.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rust;
+          cargo = rust;
+        };
+
+        # The python version we are going to use
         python = pkgs.python312;
+
+        # The Python package with our code
+        pyattimo = python.pkgs.buildPythonPackage rec {
+          pname = "pyattimo";
+          version = "0.6.0";
+          pyproject = true;
+
+          src = ./.;
+
+          cargoDeps = rustPlatform.fetchCargoVendor {
+            inherit src;
+            hash = "sha256-NVZkjf+BfUy18PAN1LF12nxaupI2/u/+SVvwU67u5dc=";
+          };
+
+          buildAndTestSubdir = "pyattimo";
+          nativeBuildInputs = with rustPlatform; [
+            rust # this is necessary to actually build using the nightly release
+            cargoSetupHook
+            maturinBuildHook
+          ];
+        };
+
+        # The Python version to put in the container, including all the
+        # necessary packages
+        containerPython = python.withPackages (ppkgs: [
+          ppkgs.numpy
+          ppkgs.scipy
+          ppkgs.icecream
+          pyattimo
+        ]);
+
+        # The apptainer container
+        container = pkgs.singularity-tools.buildImage {
+          name = "attimo";
+          contents = [containerPython];
+          diskSize = 1024 * 2; # necessary to fit the packages, otherwise the build fails
+        };
       in {
+        packages.default = container;
+
         devShells.default = pkgs.mkShell {
           venvDir = ".venv";
           buildInputs = with pkgs;
             [
-              rust-bin.nightly.latest.default
+              rust
               python
               heaptrack
             ]
